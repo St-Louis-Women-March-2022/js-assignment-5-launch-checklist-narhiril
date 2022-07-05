ad8e5ca34d57a9d98c93197588b4520e0f15df62
narhiril <roxannarusbarsky@yahoo.com>
2022-07-05 15:56:17 -0500

    work on form validation

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Write your JavaScript code here!
 
+const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
+
+    const launchChecklist = {
+        pilot: {name: '', isReady: false},
+        copilot: {name: '', isReady: false},
+        fuelLevel: {value: '', isReady: false},
+        cargoLevel: {value: '', isReady: false}
+    }
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event){
+        let pilotName = document.querySelector('input[name=pilotName]');
+        let copilotName = document.querySelector('input[name=copilotName]');
+        let fuelLevel = document.querySelector('input[name=fuelLevel]');
+        let cargoLevel = document.querySelector('input[name=cargoLevel]');
+        if (formSubmission(document, launchChecklist, pilotName, copilotName, fuelLevel, cargoLevel)){
+
+        } else {
+
+        }
+    });
+    
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,7 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -17,12 +18,74 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === ''){
+        return "Empty";
+    } else {
+        const isNonNumeric = /[^\-\.0-9e]|e(?=.*e)|\.(?=.*\.)|^e/i; //should catch everything that isNaN doesn't
+        return ((isNonNumeric.test(testInput) || isNaN(testInput) ? "Not a Number" : "Is a Number"));
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    const threshold = {fuel: 10000, cargo: 10000};
+
+    if (validateInput(pilot) === "Empty"){
+        list.pilot.isReady = false;
+        //update page to show invalid name
+    } else {
+        list.pilot.name = pilot;
+        list.pilot.isReady = true;
+        //remove invalid name prompt from page
+    }
+
+    if (validateInput(copilot) === "Empty"){
+        list.copilot.isReady = false;
+        //update page to show invalid name
+    } else {
+        list.copilot.name = pilot;
+        list.copilot.isReady = true;
+        //remove invalid name prompt from page
+    }
+
+    if (validateInput(fuelLevel) === "Is a Number"){
+        fuelLevel = parseFloat(fuelLevel);
+        list.fuelLevel.value = fuelLevel;
+        if (fuelLevel >= threshold.fuel){
+            list.fuelLevel.isReady = true;
+            //remove insufficient fuel prompt from page
+        } else {
+            list.fuelLevel.isReady = false;
+            //update page to show insufficient fuel
+        }
+    } else {
+        list.fuelLevel.isReady = false;
+        //update page to show invalid fuel entry
+    }
+    
+    if (validateInput(cargoLevel) === "Is a Number"){
+        cargoLevel = parseFloat(cargoLevel);
+        list.cargoLevel.value = cargoLevel;
+        if (cargoLevel <= threshold.cargo){
+            list.cargoLevel.isReady = true;
+            //remove too much cargo prompt from page
+        } else {
+            list.cargoLevel.isReady = false;
+            //update page to show too much cargo
+        }
+    } else {
+        list.cargoLevel.isReady = false;
+        //update page to show invalid cargo entry
+    }
+
+    for (const item of list) {
+        if (!item.isReady) {
+            return false;
+        }
+    }
+    return true;
+
 }
+
 
 async function myFetch() {
     let planetsReturned;
